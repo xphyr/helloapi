521f71f9a0a41c2d8cd9d40103983e33e457cc24
Mark DeNeve <xphyr@users.noreply.github.com>
2020-08-19 16:20:53 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hello World API app
+
+This is a small go application that does one thing... says "Hello world from \<OS Goes Here\>" where OS is the value of runtime.GOOS.
+
+Why would I want to do such a thing ... I have my reasons :)
+
---
+++ main.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2018 Mark DeNeve.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+var (
+	buf        bytes.Buffer
+	logger     = log.New(&buf, "logger ", log.Lshortfile)
+	myHostname = os.Getenv("HOSTNAME")
+	myOS       = runtime.GOOS
+)
+
+func init() {
+	flag.Parse()
+	logger.SetOutput(os.Stderr)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte(`<html>`)) // Begin html block
+	w.Write([]byte(`<head> 
+		<title>Simple K8s Test Pod</title>
+		<style>
+		label{
+		display:inline-block;
+		width:75px;
+		}
+		form label {
+		margin: 10px;
+		}
+		form input {
+		margin: 10px;
+		}
+		</style>
+		</head><body>`)) // Head  Begin Body
+	w.Write([]byte(fmt.Sprintf("<h3>Hello from: %s</h3>", myOS)))
+	w.Write([]byte(fmt.Sprintf("<h3>My hostname: %s</h3>", myHostname)))
+	w.Write([]byte(`</body></html>`)) //END
+
+}
+
+func main() {
+
+	logger.Printf("Starting App")
+	http.HandleFunc("/", rootHandler)
+	listenPort := ":8080"
+	logger.Printf("Listening on port: %v", listenPort)
+	http.ListenAndServe(listenPort, nil)
+}
